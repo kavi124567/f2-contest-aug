a22555abe457fc39fe6bb0b3db8672a637384e19
kavi124567 <120807307+kavi124567@users.noreply.github.com>
2023-08-12 23:34:27 +0530

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,36 @@
+let arr = [
+  { id: 1, name: 'john', age: 18, profession: 'developer' },
+  { id: 2, name: 'jack', age: 20, profession: 'developer' },
+  { id: 3, name: 'karen', age: 19, profession: 'admin' },
+];
+
+// Don't worry about consoling these functions, they are already being called on the button clicks.
+// Please don't change anything in the index.html file.
+
+// const developer = arr.filter(val => val.profession == "developer");
+
+// console.log(developer);
+
+  
+  // adding the new object line into array
+// arr.splice(4,0,[{id:4,name:"susan",age:"20",profession:"intern"},]);
+// console.log(arr);
+
+
+// removing object under array
+  // arr.splice(2);
+  // console.log(arr);
+
+
+// function concatenateArray() {
+//   //Write your code here, just console.log
+// }
+
+// // Here is an example of how functions work,
+// // basically a function is a block of code which can directly access your 'arr' variable since arr is global.
+// // If I have a function called consoleArr(), that can directly access arr like this to console it.
+
+// function consoleArr() {
+//   console.log('Consoling Array Variable', arr);
+//   // Over here I can directly access the variable.
+// }
